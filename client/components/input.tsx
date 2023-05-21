00145aa00eaa008ada9c8fae3a19282ab73af009
Douglinhas <daguiaralcantara@gmail.com>
2023-05-21 14:47:30 -0300

    Criação da função handle dos formulários.

--- client/components/input.tsx
+++ client/components/input.tsx
@@ -1,12 +1,15 @@
+import { ChangeEvent } from "react";
 import styles from "../styles/components/input.module.css";
 
 type Props = {
   type: string
   placeholder: string
+  value: string
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 } 
 
 export default function Input(props: Props) {
   return (
-    <input className={ styles.input } {...props} />
+    <input className={ styles.input } {...props} required />
   )
 }
--- client/pages/login.tsx
+++ client/pages/login.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent, useState } from "react";
 import styles from "../styles/pages/login.module.css";
 import Link from "next/link";
 
@@ -6,12 +7,35 @@ import Input from "../components/input";
 import LoginCard from "../components/loginCard";
 
 export default function LoginPage() {
+  const [formData, setFormaData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleFormData = (e: ChangeEvent<HTMLInputElement>, name: string) => {
+    setFormaData({
+      ...formData,
+      [name]: e.target.value
+    })
+  }
+
   return (
     <div className={ `${styles.background} ${styles.card}` }>
       <LoginCard title="Faça seu Login">
         <form className={ styles.form }>
-          <Input type="text" placeholder="Seu e-mail:" />
-          <Input type="password" placeholder="Sua senha:" />
+        <Input
+            type="text"
+            placeholder="Seu e-mail:"
+            value={ formData.email }
+            onChange={ (e: ChangeEvent<HTMLInputElement>) => { handleFormData(e, "email") } }
+            />
+          <Input
+            type="password"
+            placeholder="Sua senha:"
+            value={ formData.password }
+            onChange={ (e: ChangeEvent<HTMLInputElement>) => { handleFormData(e, "password") } }
+          />
           <Button>Login</Button>
           <p className={ styles.p }>Ainda não possui conta? {" "}<Link href="/cadastro" className={ styles.link }>Cadastrar</Link></p>
         </form>

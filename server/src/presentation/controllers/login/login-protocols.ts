29b8ee996d69704c334a98299a7923617a272fb1
Douglinhas <daguiaralcantara@gmail.com>
2023-05-21 20:24:11 -0300

    Implementação do LoginController.

---
+++ server/src/presentation/controllers/login/login-protocols.ts
@@ -0,0 +1,5 @@
+export * from "../../../domain/useCases/login";
+export * from "../../errors";
+export * from "../../helpers/http-helper";
+export * from "../../protocols/controller";
+export * from "../../protocols/http";
---
+++ server/src/presentation/controllers/login/login.ts
@@ -0,0 +1,21 @@
+import { Controller, HttpRequest, HttpResponse, ILogin, InvalidParamError, badRequest, ok, serverError } from "./login-protocols";
+
+export class LoginController implements Controller {
+  constructor(private login: ILogin) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { email, password } = httpRequest.body;
+
+      if (password.length < 6) {
+        return badRequest(new InvalidParamError("password", "Senha com menos de 6 caracteres!"));
+      }
+
+      const token = await this.login.handle({ email, password });
+
+      return ok(token);
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
